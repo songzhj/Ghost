8fbf303279c2e262533e90632208b12c96d79246
宋治佳 <zhijia.song@qunar.com>
2017-11-04 18:03:58 +0800

    change the theme code format

--- content/themes/ghostiumV2/src/assets/js/imageLoader.js
+++ content/themes/ghostiumV2/src/assets/js/imageLoader.js
@@ -7,28 +7,28 @@ var ImageLoader = (function ($) {
         _overlay = $('.drawer-overlay');
 
     var module = {
-        done: function(e) {
-          e.addClass('image-loaded');
+        done: function (e) {
+            e.addClass('image-loaded');
         },
-        success: function(e, image) {
-          e.css('background-image', 'url(' + image.src + ')');
-          module.done(e);
+        success: function (e, image) {
+            e.css('background-image', 'url(' + image.src + ')');
+            module.done(e);
         },
-        init: function($el) {
-          var image = new Image(),
-              src = $el.data('load-image');
+        init: function ($el) {
+            var image = new Image(),
+                src = $el.data('load-image');
 
-          if(src.length) {
-            image.src = src;
-            image.onload = module.success($el, image);
-            image.onerror = module.done($el);
-            image.onabort = module.done($el);
-          }
+            if (src.length) {
+                image.src = src;
+                image.onload = module.success($el, image);
+                image.onerror = module.done($el);
+                image.onabort = module.done($el);
+            }
 
-          return image;
+            return image;
         }
     };
 
-    return { load: module.init };
+    return {load: module.init};
 
 }(jQuery));
